0c1ea3a12dbcdc22ddbec09844ccdf03a9482b0c
Joona Heinikari <joona.heinikari@elisanet.fi>
2018-05-15 13:34:26 +0300

    Contacts-webapp added first list of contacts

---
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {Contact} from '../contact';
+
+@Component({
+  selector: 'cw-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+
+  contacts: Contact[];
+
+  constructor() {
+    this.contacts = [];
+  }
+
+  ngOnInit() {
+    this.contacts.push(new Contact('Mikki', 'Hiiri'));
+    this.contacts.push(new Contact('Aku', 'Ankka'));
+    this.contacts.push(new Contact('Teppo', 'Tulppu'));
+    console.log(this.contacts);
+  }
+
+}
